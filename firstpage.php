57740022637f00bad766a98fa4438ac91ed4e642
mercuryshashank <32571082+mercuryshashank@users.noreply.github.com>
2019-09-22 13:10:57 +0530

    Create firstpage.php

---
+++ firstpage.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Trekking Websites</title>
+<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+<header>
+	<div class="main">
+		<div class="logo">
+		<img src="logo.png">
+		</div>
+		<ul>
+			<li class="active"><a href="#">Home</a></li>
+			<li><a href="register.php">Register now</a></li>
+			<li><a href="startpage.php">Admin Login</a></li>
+			<li><a href="review.php">Review</a></li>
+		</ul>
+	</div>
+	<div class="title">
+	<h1>Trekking Website</h1></div>
+</header>
+</body>
+</html>
