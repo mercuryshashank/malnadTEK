3ba1d012181a982b0f40986229b6e34d5d7b296f
mercuryshashank <32571082+mercuryshashank@users.noreply.github.com>
2019-09-22 13:05:45 +0530

    Create status.php

---
+++ status.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Trekking Website</title>
+	<link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+<header>
+	<div class="main">
+		<div class="logo">
+		<img src="logo.png">
+		</div>
+		<ul>
+			<li><a href="firstpage.php">Home</a></li>
+			<li class="active"><a href="register.php">Register now</a></li>
+			<li><a href="startpage.php">Admin Login</a></li>
+			<li><a href="review.php">Review</a></li>
+		</ul>
+	</div>
+	<div class="title">
+	<h1> payment sucessfull</h1>
+	<h1>for next booking <a href="startpage.php"> Start</h1>
+	</div>
+</header>
+</div>
+</body>
+</html>
